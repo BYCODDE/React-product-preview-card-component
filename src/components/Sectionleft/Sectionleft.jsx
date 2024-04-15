0f5bdf6764cf7956bf36014285bc81660fcdd4e7
BYCODDE <116018979+BYCODDE@users.noreply.github.com>
2024-04-15 19:20:24 +0400

    bottom section left!

--- src/components/Sectionleft/Sectionleft.jsx
+++ src/components/Sectionleft/Sectionleft.jsx
@@ -1,12 +1,7 @@
 import "./sectionleft.css";
 // import desktop from "/images/image-product-desktop.jpg";
 function Sectionleft() {
-  return (
-    <section className="section_left">
-      {/* <img src={desktop} alt="product-desktop-img" /> */}
-      
-    </section>
-  );
+  return <section className="section_left"></section>;
 }
 
 export default Sectionleft;
